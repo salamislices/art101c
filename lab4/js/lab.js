bd2628afd5d271322ccb52fbca7fe1f8676adca9
danielf <freyser5625@gmail.com>
2025-04-19 15:51:17 -0700

    edits

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
 // Author: Daniel Freysson
-// Date:
+// Date:4/19/25
 
 // Constants
 
@@ -8,7 +8,18 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
+//1. Display a hanging post
+//2. Player 1 inputs a word
+//3. # of lines generate based on the input word length
+//4. Player 2 inputs a guess letter
+  //a. If input is in chosen word
+    //i. Display the letter in the lines in the position it is in in the word
+  //b. Else: not in the chosen word:
+    //i. Display a new body part on the hanging post
+//5. If the hanging post has a complete man on it:
+  //a. Player 2 loses
+//6. If the word is guessed before there is a complete man on the hang post:
+  //a. Player 2 wins
   // return results;
 }
 
