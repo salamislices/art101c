8f0c4e90300abd2c3bc81a1ab88c7e344e90bdd8
danielf <freyser5625@gmail.com>
2025-04-28 16:48:35 -0700

    lab6 edits

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 6: Objects and Arrays
+// Author: Daniel Freysson
+// Date: 4/28/25
+
+// Define Variables
+let myTransport = ["car", "bicycle", "bus", "walking"];
+
+// Create an Object for Main Ride
+var myMainRide = {
+  make: "Toyota", 
+  model: "Celica", 
+  color: "Silver", 
+  year: 2000, 
+  age: function() 
+    {return 2025 - this.year;}
+  } 
+
+//output
+document.writeln("How I tend to get around: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+
+
+
+
+
 
 // Constants
 
