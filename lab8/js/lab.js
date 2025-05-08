cbc1e85eb245cf470f61c636553a1d10e32078a1
danielf <freyser5625@gmail.com>
2025-05-08 14:20:03 -0700

    final changes lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,6 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 8 - Anon Functions and Callbacks
+// Author: Daniel
+// Date:5/8/25
+
+function squareTen(x){
+    var results = (x * x) + 10;
+    return results;
+} 
+
+// test
+console.log("What is 20 squared plus 10? ", squareTen(20));
+console.log("What is 5 squared plus 10? ", squareTen(5));
+
+coolArray = [3, 4, 5, 6, 9, 12, 20] 
+console.log("My cool array: ", coolArray);
+
+var result = coolArray.map(squareTen);
+// should return [19, 26, 35, 46, 91, 154, 410]
+console.log("Test of squareTen function on array: ", result);
+
+var result = coolArray.map(function(x) {
+    return result = x + 3 * 1111;
+})
+// should return [3336, 3337, 3338, 3339, 3342, 3345, 3353]
+console.log("Array plus 3 times 1111: ", result);
+
 
 // Constants
 
