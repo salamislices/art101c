1e382abf7dc2067d7f5d526af34a3a9d6456631b
danielf <freyser5625@gmail.com>
2025-05-15 14:21:36 -0700

    new

---
+++ lab10/js/lab.js
@@ -0,0 +1,43 @@
+// Lab 10 - JavaScript for the Web
+// Author: Daniel
+// Date:5/15/25
+
+// Constants
+
+// Functions
+
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+
+// click listener for button
+$("#make-convo").click(function(){
+});
+
+// get new fake dialogue
+const newText = generateRandomText();
+
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
