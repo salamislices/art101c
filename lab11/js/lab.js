b67ec52cd28900f6176a355a6c0cf6feb1bf1b59
danielf <freyser5625@gmail.com>
2025-05-19 16:19:48 -0700

    lab 11 edit

---
+++ lab11/js/lab.js
@@ -0,0 +1,33 @@
+// Lab 11 - JavaScript Events and Forms
+// Author: Daniel
+// Date:5/19/25
+
+// Constants
+
+// Functions
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
+}
+
+$("#submit").click(function(){
+    const userName = $("#user-name").val();
+    userNameSorted = sortString(userName);
+});
+
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
