1c92706bd4eec372e2104120bba4a11566c77198
danielf <freyser5625@gmail.com>
2025-05-22 14:06:17 -0700

    edit

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -2,21 +2,37 @@
 // Author: Daniel
 // Date:5/22/25
 
-// Constants
 
-// Functions
+// This function takes a string and returns a house based on string length
+function sortingHat(str) {
+    let length = str.length;           
+    let mod = length % 4;             
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-    // some code here
-    // return results;
-  }
-  
-  function main() {
+// Return a house depending on the mod result
+    if (mod === 0) {
+        return "Gryffindor";
+    } else if (mod === 1) {
+        return "Ravenclaw";
+    } else if (mod === 2) {
+        return "Hufflepuff";
+    } else if (mod === 3) {
+        return "Slytherin";
+    }
+}
+
+// Main function to run when page is ready
+function main() {
     console.log("Main function started.");
-    // the code that makes everything happen
-  }
-  
-  // let's get this party started
-  main();
-  
+
+    // Add a click event listener to the button
+    $("#button").click(function() {
+        let name = $("#input").val();           // Get value from input field
+        let house = sortingHat(name);           // Run sortingHat to get house
+        $("#output").append(                    // Display result in #output
+            `<p>The Sorting Hat has sorted you into <strong>${house}</strong>!</p>`
+        );
+    });
+}
+
+// Let's get this party started
+main();
