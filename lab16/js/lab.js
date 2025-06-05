23c7007988a75e673bedec69b4b84866c4cd6b92
danielf <freyser5625@gmail.com>
2025-06-05 13:14:29 -0700

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,22 +1,15 @@
-// Lab 16 - JSON and APIs
-// Author: Daniel
-// Date: 6/5/25
-
-// This script uses jQuery to call the XKCD API and display the latest comic on the page
-
-// Start when the document is fully loaded
 $(document).ready(function () {
-    console.log("Document loaded. Starting AJAX call...");
+    console.log("Document loaded...");
 
-    // Make an AJAX request to the XKCD API
     $.ajax({
-        url: "https://xkcd.com/info.0.json", // URL to fetch the latest comic
+        url: "https://api.allorigins.win/get?url=https://xkcd.com/info.0.json",
         type: "GET",
         dataType: "json",
-        success: function (comicObj) {
-            console.log("API success:", comicObj); // See the full JSON object
+        success: function (response) {
+            const comicObj = JSON.parse(response.contents);
 
-            // Create elements for the comic title, image, and alt text
+            console.log("API success:", comicObj);
+
             const title = $("<h3>").text(comicObj.title);
             const image = $("<img>")
                 .attr("src", comicObj.img)
@@ -25,7 +18,6 @@ $(document).ready(function () {
                 .css("max-width", "100%");
             const caption = $("<p>").text(comicObj.alt);
 
-            // Append to #output div
             $("#output").append(title, image, caption);
         },
         error: function (jqXHR, textStatus, errorThrown) {
